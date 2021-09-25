9cd8a8e1599a854b900243e157bfabeb73ea9ff0
NLogDEV <85012778+NLogDEV@users.noreply.github.com>
2021-09-25 14:54:48 +0700

    Update FileManager.py

--- FileManager.py
+++ FileManager.py
@@ -3,6 +3,7 @@
 	By NlogDev
 """
 
+
 from tkinter import *
 from tkinter import simpledialog, messagebox, ttk
 import shutil        
@@ -16,13 +17,14 @@ except ImportError:
 from pathlib import Path
 import win32api
 
-version = "1.3.0.2"
+version = "1.3.1"
 
 root = Tk()
 
-clipBoard = None
+clipBoard = []
 
 state = False
+cutprocess = True
 
 fileListBox = None
 textArea = None
@@ -96,7 +98,8 @@ def file_size(file_path):
         file_info = os.stat(file_path)
         return convert_bytes(file_info.st_size)
 
-
+#All control
+#WARN: NO multiple selection.
 def newFile(parent):
 	if curPathText.get() == "System Directory":
 		pass
@@ -141,7 +144,7 @@ def renameSelectedFile(parent):
 	else:
 		try:
 			fileName = curPathText.get() + "\\" + fileListBox.focus()
-			in_ = get_valid_filename(simpledialog.askstring("Input", "Please enter the new file name:", parent=parent))
+			in_ = get_valid_filename(simpledialog.askstring("Input", "Please enter the new file name: \n *Note: You Can't rename multiple files*", parent=parent))
 			if(in_ is None):
 				pass
 			if "." in in_:
@@ -160,49 +163,96 @@ def deleteSelectedFile():
 		pass
 	else:
 		try:
-			fileName = fileListBox.focus()
-			confirmDelete = messagebox.askyesno("","Confirm to delete " + fileName + "?")
+			ex = 0
+			multipleappname = fileListBox.selection()
+			for count in multipleappname:
+				ex += 1
+				
+			confirmDelete = messagebox.askyesno("","Confirm to delete " + str(ex) + " Files?")
 			if(confirmDelete):
-				os.remove(curPathText.get() + "\\" + fileName)
-				reloadFiles(0)
+				for filee in multipleappname:
+					os.remove(curPathText.get() + "\\" + filee)
+					reloadFiles(0)
 		except (TclError, PermissionError) as e:
-			messagebox.showerror("Error to delete '" + (fileName) + "'", "Error: " + str(e))
+			messagebox.showerror("Error to delete '" + fileListBox.selection() + "'", "Error: " + str(e))
 
 def upward():
 	curPathText.set(('\\').join(curPathText.get().split("\\")[:-1]))
 	os.chdir("..")
 	reloadFiles()
 
+#Copy/Cut/Paste function
+#WARN: NO multiple selection.
 def copy():
 	global clipBoard
 	global transferMode
+	clipBoard = [] #Make sure to clear the Clipboard
+	multiplefilename = fileListBox.selection()
 	if curPathText.get() == "System Directory":
 		pass
 	else:
 		try:
-			clipBoard = curPathText.get() + "\\" + fileListBox.focus()
+			for fn in multiplefilename:
+				clipBoard.append(curPathText.get() + "\\" + str(fn))
+				print("Added " + curPathText.get() + "\\" + str(fn) + "To clipboard.")
+				print("Copied", fn)
 			transferMode = "copy"
-			print("Copied", clipBoard)
 		except TclError:
 			pass
 
 def cut():
 	global clipBoard
 	global transferMode
+	multipleappname = fileListBox.selection()
 	if curPathText.get() == "System Directory":
 		pass
 	else:
 		try:
-			if fileListBox.focus() == "":
-				print("a")
-			else:
-				clipBoard = curPathText.get() + "\\" + fileListBox.focus()
-				transferMode = "cut"
-				print("Cut", clipBoard)
-				fileListBox.delete(fileListBox.focus())
+			#Make sure to clear the clipboard
+			clipBoard = []
+			ex = 0
+			transferMode = "cut"
+			for a in multipleappname:
+				ex += 1
+				clipBoard.append(curPathText.get() + "\\" + str(a))
+				print("Added " + curPathText.get() + "\\" + str(a) + "To clipboard.")
+				fileListBox.delete(a)
+				print("Cut", a)
 		except TclError:
 			pass
-		
+
+def paste():
+	global clipBoard
+	global transferMode
+	global cutprocess
+	try:
+		if curPathText.get() == "System Directory":
+			pass
+		else:
+			for fn in clipBoard:
+				fileName = fn.split("\\")[-1]
+				destination = curPathText.get() + "\\" + fileName
+				if fn == destination:
+					pass
+				else:
+					if(transferMode == "cut"):
+						cutprocess = True
+						shutil.move(fn, destination)
+						notif = "Moving"
+					elif(transferMode == "copy"):
+						shutil.copyfile(fn, destination)
+						notif = "Copying"
+					print("Paste", notif + " " + fn + " to " + destination)
+			if notif == "Moving":
+				clipBoard = []
+				cutprocess = False
+	except Exception as e:
+		if clipBoard == None:
+			pass
+		else:
+			messagebox.showerror(clipBoard, "Error: " + str(e))
+	reloadFiles(0)
+
 def check_for_updates():
 	try:
 		import requests
@@ -219,40 +269,6 @@ def check_for_updates():
 			messagebox.showinfo("New Version!", "Your Current version is " + version +" Please update to new version: " + checkversion)
 	except:
 		messagebox.showerror("Error", "Please connect to the network")
-
-def paste():
-	global clipBoard
-	global transferMode
-	try:
-		if curPathText.get() == "System Directory":
-			pass
-		else:
-			fileName = clipBoard.split("\\")[-1]
-			try:
-				if(transferMode == "copy"):
-					[fileName, fileType] = fileName.split(".")
-					fileName = fileName + "_copy." + fileType
-			except:
-				pass
-			destination = curPathText.get() + "\\" + fileName
-			if clipBoard == destination:
-				pass
-			else:
-				if(transferMode == "cut"):
-					shutil.move(clipBoard, destination)
-					notif = "Moving"
-				elif(transferMode == "copy"):
-					shutil.copyfile(clipBoard, destination)
-					notif = "Copying"
-				messagebox.showinfo("Paste", notif + " " + clipBoard + " to " + destination)
-				if notif == "Moving":
-					clipBoard = None
-	except Exception as e:
-		if clipBoard == None:
-			pass
-		else:
-			messagebox.showerror(clipBoard, "Error: " + str(e))
-	reloadFiles(0)
 
 def about():
 	messagebox.showinfo("About", "📁 File Manager " + version + "\n Made by LNogDEV.")
@@ -321,7 +337,7 @@ def reloadFiles(fileToSelect = None):
 					selectionInd = added
 			if file_size(item) is not None:
 				name, extension = os.path.splitext(item)
-				fileListBox.insert("","end",iid= item ,values = (item, file_size(item), extension + " File" ,time.ctime(os.path.getmtime(item))))
+				fileListBox.insert("","end",iid= item ,values = (item, time.ctime(os.path.getmtime(item)) , extension + " File" , file_size(item)))
 			else:
 				fileListBox.insert("","end",iid= item ,values = (item, "", "Directory" ,time.ctime(os.path.getmtime(item))))
 			added += 1
@@ -334,6 +350,7 @@ def opensystem(event):
 	global curPathText
 	oldpath = os.getcwd()
 	fileName = fileListBox.focus()
+	multiplefile = fileListBox.selection()
 	try:
 		filePath = curPathText.get() + "\\" + fileName
 		if ":" in fileName:
@@ -351,7 +368,9 @@ def opensystem(event):
 				try:
 					curPathText.set(oldpath)
 					ChangeText(oldpath)
-					os.startfile(filePath)
+					for f in multiplefile:
+						multiplefilePath = curPathText.get() + "\\" + str(f)
+						os.startfile(multiplefilePath)
 				except OSError:
 					messagebox.showerror("Error", "Unable to enter folder or run file named: " + fileName)
 	except Exception as e:
@@ -367,6 +386,8 @@ def go(event):
 			sysdir()
 		elif "maindir" == pll.get():
 			sysdir()
+		elif "\\" not in pll.get():
+			ChangeText(lastdir)
 		elif "." in pll.get():
 			os.startfile(pll.get)
 			ChangeText(lastdir)
@@ -380,12 +401,12 @@ def go(event):
 		
 def showfileinfo():
 	appName = fileListBox.focus()
-	name, extension = os.path.splitext(appName)
-	curAppdir = curPathText.get() + "\\" + appName
+	multipleappname = fileListBox.selection()
 	window = Toplevel(root)
 	window.title(appName)
 	window.geometry("300x250")
 	window.resizable(0,0)
+	curAppdir = curPathText.get() + "\\" + appName
 	apptext = Label(window, text="📁 " + appName + "\n")
 	apptext.pack(side=TOP)
 	typetext = Label(window, text="\nType of file: ")
@@ -394,7 +415,8 @@ def showfileinfo():
 	sizetext.pack(side=TOP)
 	separator = Label (window, text=" ——————————————\n")
 	separator.pack(side=TOP)
-
+	ex = 0
+	name, extension = os.path.splitext(appName)
 	try:
 		try:
 			appsize = file_size(curAppdir)
@@ -416,25 +438,37 @@ def showfileinfo():
 		except (OSError, PermissionError, TclError):
 			sizetext.configure(text="Cannot detect the size")
 			typetext.configure(text="")
+	
+	for count in multipleappname:
+		ex += 1
+		if ex >= 1:
+			window.title(str(ex) + " files selected")
+			apptext.configure(text=str(ex) + " files selected")
+			sizetext.configure(text="")
+			typetext.configure(text="Type: Multiple types")
 
 	try:
 		try:
-			ctime = Label(window, text="Created: %s" % time.ctime(os.path.getctime(appName)))
-			ctime.pack(side=TOP)
-			mtime = Label(window, text="Modified: %s" % time.ctime(os.path.getmtime(appName)))
-			mtime.pack(side=TOP)
-			atime = Label(window, text="Accessed: %s" % time.ctime(os.path.getatime(appName)))
-			atime.pack(side=TOP)
+			if ex >= 1:
+				pass
+			else:
+				ctime = Label(window, text="Created: %s" % time.ctime(os.path.getctime(appName)))
+				ctime.pack(side=TOP)
+				mtime = Label(window, text="Modified: %s" % time.ctime(os.path.getmtime(appName)))
+				mtime.pack(side=TOP)
+				atime = Label(window, text="Accessed: %s" % time.ctime(os.path.getatime(appName)))
+				atime.pack(side=TOP)
 		except PermissionError:
 			pass
 	except FileNotFoundError:
-		window.destroy()
+		pass
 
 frame1=Frame(root)
 frame1.pack(side=TOP,fill=X)
 
 file_mgr = root
-root.after(10000, reloadFiles)
+root.after(20000, reloadFiles)
+
 file_mgr.title("File Manager")
 file_mgr.geometry("800x500")
 
@@ -466,18 +500,18 @@ m = PanedWindow(file_mgr,orient="horizontal")
 m.pack(fill=BOTH ,expand=1)
 
 fileListBox = ttk.Treeview(m, name='fileListBox')
-fileListBox['columns']=('name', 'size', 'type', 'datem')
+fileListBox['columns']=('name', 'datem', 'type', 'size')
 fileListBox.column('#0', width=0, stretch=NO)
 fileListBox.column('name',  width=80)
-fileListBox.column('size',  width=80)
 fileListBox.column('datem',  width=80)
 fileListBox.column('type',  width=80)
+fileListBox.column('size',  width=80)
 
 fileListBox.heading('#0')
 fileListBox.heading('name', text='Name')
-fileListBox.heading('size', text='Size')
 fileListBox.heading('datem', text='Date modified')
 fileListBox.heading('type', text='Type')
+fileListBox.heading('size', text='Size')
 
 fileListBox.bind('<<ListboxSelect>>')
 fileListBox.bind("<Double-Button-1>", opensystem)
@@ -486,9 +520,6 @@ fileListBox.bind('<Control-Key-V>', paste)
 
 scrollbar = Scrollbar(m)
 scrollbar.pack(side = RIGHT, fill = BOTH)
-
-for values in range(-1):
-	fileListBox.insert(END, values)
 
 fileListBox.config(yscrollcommand = scrollbar.set)
 scrollbar.config(command = fileListBox.yview)
